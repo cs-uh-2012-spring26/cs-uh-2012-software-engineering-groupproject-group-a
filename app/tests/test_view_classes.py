03e82bf69e06151f76ca4e24e69898618caf69fc
Tokla Roeum <tr2458@nyu.edu>
2026-03-13 19:42:36 +0700

    added two tests for view class endpoint

--- app/tests/test_view_classes.py
+++ app/tests/test_view_classes.py
@@ -1,1 +1,15 @@
-# Tokla
+def test_get_all_classes_empty(client):
+    response = client.get('/classes')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data == {"classes": []}
+
+def test_get_all_classes(client, seeded_class):
+    response = client.get('/classes')
+    assert response.status_code == 200
+    data = response.get_json()
+    assert "classes" in data
+    assert len(data["classes"]) == 1
+    assert data["classes"][0]["class_name"] == "Morning Yoga"
+    assert data["classes"][0]["location"] == "Studio A"
+    assert data["classes"][0]["capacity"] == 20
